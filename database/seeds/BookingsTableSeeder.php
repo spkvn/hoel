7409a496b1b74c40f8f340f621dda5428746f4e8
spkvn <kevinmeagher1@hotmail.com>
2017-09-14 20:42:27 +1000

    added more seeds, test data for patron

--- database/seeds/BookingsTableSeeder.php
+++ database/seeds/BookingsTableSeeder.php
@@ -33,10 +33,52 @@ class BookingsTableSeeder extends Seeder
         ]);
 
         DB::table('bookings')->insert([
-        	'room_id'  => '4',	
-        	'user_id'  => '7',	
-        	'check_in' => '2017/08/14',
-        	'check_out'=> '2017/08/16'
+            'room_id'  => '4',  
+            'user_id'  => '7',  
+            'check_in' => '2017/08/14',
+            'check_out'=> '2017/08/16'
+        ]);
+        DB::table('bookings')->insert([
+            'room_id'  => '4',  
+            'user_id'  => '8',  
+            'check_in' => '2017/08/14',
+            'check_out'=> '2017/08/16'
+        ]);        
+        DB::table('bookings')->insert([
+            'room_id'  => '4',  
+            'user_id'  => '9',  
+            'check_in' => '2017/08/17',
+            'check_out'=> '2017/08/18'
+        ]);        
+        DB::table('bookings')->insert([
+            'room_id'  => '4',  
+            'user_id'  => '10',  
+            'check_in' => '2017/08/19',
+            'check_out'=> '2017/08/20'
+        ]);        
+        DB::table('bookings')->insert([
+            'room_id'  => '4',  
+            'user_id'  => '11',  
+            'check_in' => '2017/08/21',
+            'check_out'=> '2017/09/12'
+        ]);        
+        DB::table('bookings')->insert([
+            'room_id'  => '4',  
+            'user_id'  => '12',  
+            'check_in' => '2017/09/13',
+            'check_out'=> '2018/09/16'
+        ]);        
+        DB::table('bookings')->insert([
+            'room_id'  => '4',  
+            'user_id'  => '13',  
+            'check_in' => '2018/08/14',
+            'check_out'=> '2018/08/16'
+        ]);        
+        sDB::table('bookings')->insert([
+            'room_id'  => '4',  
+            'user_id'  => '14',  
+            'check_in' => '2018/09/14',
+            'check_out'=> '2018/09/16'
         ]);
     }
 }
