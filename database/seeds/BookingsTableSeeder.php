9e8ac5f758a7741ef618267d6fb66d84ab0ebe45
spkvn <kevinmeagher1@hotmail.com>
2017-09-24 20:27:40 +1000

    fix db seed values

--- database/seeds/BookingsTableSeeder.php
+++ database/seeds/BookingsTableSeeder.php
@@ -33,50 +33,50 @@ class BookingsTableSeeder extends Seeder
         ]);
 
         DB::table('bookings')->insert([
-            'room_id'  => '4',  
-            'user_id'  => '7',  
+            'room_id'  => '7',  
+            'user_id'  => '4',  
             'check_in' => '2017/08/14',
             'check_out'=> '2017/08/16'
         ]);
         DB::table('bookings')->insert([
-            'room_id'  => '4',  
-            'user_id'  => '8',  
+            'room_id'  => '8',  
+            'user_id'  => '4',  
             'check_in' => '2017/08/14',
             'check_out'=> '2017/08/16'
         ]);        
         DB::table('bookings')->insert([
-            'room_id'  => '4',  
-            'user_id'  => '9',  
+            'room_id'  => '9',  
+            'user_id'  => '4',  
             'check_in' => '2017/08/17',
             'check_out'=> '2017/08/18'
         ]);        
         DB::table('bookings')->insert([
-            'room_id'  => '4',  
-            'user_id'  => '10',  
+            'room_id'  => '10',  
+            'user_id'  => '4',  
             'check_in' => '2017/08/19',
             'check_out'=> '2017/08/20'
         ]);        
         DB::table('bookings')->insert([
-            'room_id'  => '4',  
-            'user_id'  => '11',  
+            'room_id'  => '11',  
+            'user_id'  => '4',  
             'check_in' => '2017/08/21',
             'check_out'=> '2017/09/12'
         ]);        
         DB::table('bookings')->insert([
-            'room_id'  => '4',  
-            'user_id'  => '12',  
+            'room_id'  => '12',  
+            'user_id'  => '4',  
             'check_in' => '2017/09/13',
             'check_out'=> '2018/09/16'
         ]);        
         DB::table('bookings')->insert([
-            'room_id'  => '4',  
-            'user_id'  => '13',  
+            'room_id'  => '13',  
+            'user_id'  => '4',  
             'check_in' => '2018/08/14',
             'check_out'=> '2018/08/16'
         ]);        
         DB::table('bookings')->insert([
-            'room_id'  => '4',  
-            'user_id'  => '14',  
+            'room_id'  => '14',  
+            'user_id'  => '4',  
             'check_in' => '2018/09/14',
             'check_out'=> '2018/09/16'
         ]);
