3b7185e7c8260fa5b0c073f0c20fa84d70c8b579
spkvn <kevinmeagher1@hotmail.com>
2017-09-27 21:43:48 +1000

    inital Room Model unit tests

--- tests/Unit/RoomTest.php
+++ tests/Unit/RoomTest.php
@@ -1,67 +1,38 @@
 <?php
 
-// namespace Tests\Unit;
+namespace Tests\Unit;
 
-// use Tests\TestCase;
-// use Illuminate\Foundation\Testing\DatabaseMigrations;
-// use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-// use App\Room;
-// use App\Booking;
-// use App\Card;
-// use App\User;
+use App\Room;
 
-// class RoomTest extends TestCase
-// {
-// 	private $user; 
-// 	private $room;
-// 	private $card;
-// 	private $booking;
-// 	//sets member data
-// 	public function __construct()
-// 	{
-// 		//$pass = bcrypt('password');
-// 		$user = User::create([
-// 			'name' => 'testuser',
-// 			'email' => 'test@user.com',
-// 			'password' => 'password',
-// 			'category' => 'Administrator'
-// 		]);
-// 		$room = Room::create([
-// 			'room_number' => '9876',
-// 			'beds' => 4,
-// 			'max_capacity' => 4,
-// 			'price_per_night'=> 18
-// 		]);
-// 		$card = Card::create([
-// 			'access' => $room->id,
-// 			'user_id' => $user->id
-// 		]);
-// 		$booking = Booking::create([
-// 			'room_id' => $room->id,
-// 			'user_id' => $user->id,
-// 			'check_in' => strtotime('today'),
-// 			'check_out' => strtotime('tomorrow')
-// 		]);
-// 		echo 'Room Properties created'.PHP_EOL;
-// 	}
+class RoomTest extends TestCase
+{
+	private $room; 
+	public function __construct()
+	{
+		$this->room = new Room();
+		$this->room->id = "999";
+		$this->room->room_number = "9999";
+		$this->room->beds = 1;
+		$this->room->max_capacity = 2;
+		$this->room->price_per_night = 400;
+	}
 
-// 	//deletes member data
-// 	public function __destruct()
-// 	{
-// 		$booking->delete();
-// 		$card->delete();
-// 		$room->delete();
-// 		$user->delete();
-// 	}
+    public function testHasRoomNumber()
+    {
+    	$this->assertEquals("9999",$this->room->room_number);
+    }
 
-// 	/**
-//      * A basic test example.
-//      *
-//      * @return void
-//      */
-//     public function testExample()
-//     {
-//         $this->assertTrue(true);
-//     }
-// }
+    public function testHasId()
+    {
+    	$this->assertEquals('999',$this->room->id);
+    }
+
+    public function testHasBeds()
+    {
+    	$this->assertEquals(1, $this->room->beds);
+    }
+}
