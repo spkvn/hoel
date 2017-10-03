7b2365f04eb629ed83c600056179c29f6e94f805
spkvn <kevinmeagher1@hotmail.com>
2017-10-03 16:14:46 +1100

    little formatting to test script

--- tests/KevinCustomTest.sh
+++ tests/KevinCustomTest.sh
@@ -1,3 +1,6 @@
+echo "###############Database Migrations"
 php artisan migrate:refresh --seed
+echo "###############PHPUnit Tests"
 vendor/bin/phpunit -c phpunit.xml
+echo "###############Dusk Browser Testing"
 php artisan dusk
