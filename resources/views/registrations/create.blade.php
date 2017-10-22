45f8d33eaa6e751c8eace02a64193b69e70a937a
spkvn <kevinmeagher1@hotmail.com>
2017-10-22 20:02:15 +1100

    starting to mess around with sass

--- resources/views/registrations/create.blade.php
+++ resources/views/registrations/create.blade.php
@@ -1,89 +1,92 @@
 @extends('layouts.master')
 @section('content')
-	<div class="col-sm-12">
-
-		<h1>Register</h1>
-	
-		<hr>
-	
-		<form method="POST" action="/register">
-			
-			{{csrf_field()}}
-			
-			<!--User Name-->
-			<div class="form-group">
-				<label for="name">Name:</label>
-				<input type="text" class="form-control" id="name" name="name" 
-					   value="{{old('name')}}" required>
+	<div class="col-sm-12 login__container">
+		<div class="col-sm-4"></div>
+		<div class="col-sm-4 centre-block login__div">
+			<div class="col-xs-12 hotel__img short__div">
+				<h1>Register</h1>
 			</div>
+			<hr>
 
-			<!--User Email-->
-			<div class="form-group">
-				<label for="email">Email:</label>
-				<input type="email" class="form-control" id="email" name="email"
-					   value="{{old('email')}}" required>
-			</div>
+			<form method="POST" action="/register">
 
-			<!--First Password-->
-			<div class="form-group">
-				<label for="password">Password:</label>
-				<input type="password" class="form-control" id="password" name="password" 
-					   value="{{old('password')}}" required>
-			</div>
+				{{csrf_field()}}
 
-			<!--Confirmation Password-->
-			<div class="form-group">
-				<label for="password_confirmation">Password Confirmation:</label>
-				<input type="password" class="form-control" id="password_confirmation" name="password_confirmation" value="{{old('password_confirmation')}}" required>
-			</div>
+				<!--User Name-->
+				<div class="form-group">
+					<label for="name">Name:</label>
+					<input type="text" class="form-control" id="name" name="name"
+						   value="{{old('name')}}" required>
+				</div>
 
-			<!--User Category-->
-			<div class="form-group">
-				<label for="category">User Category:</label>
-				<select class="form-control" name="category" id="category">
-					<option value="Administrator"
-					@if(old('category')=='Administrator')
-						selected="selected"
-					@endif
-					>
-						Administrator
-					</option>
-					<option value="Reception"
-					@if(old('category')=='Reception')
-						selected="selected"
-					@endif
-					>
-						Reception
-					</option>
-					<option value="Housekeeping"
-					@if(old('category')=='Housekeeping')
-						selected="selected"
-					@endif>
-						Housekeeping
-					</option>
-					<option value="Patron"
-					@if(old('category')=='Patron')
-						selected="selected"
-					@endif>
-						Patron
-					</option>
-				</select>
-			</div>
+				<!--User Email-->
+				<div class="form-group">
+					<label for="email">Email:</label>
+					<input type="email" class="form-control" id="email" name="email"
+						   value="{{old('email')}}" required>
+				</div>
 
-			<div class="form-group">
+				<!--First Password-->
+				<div class="form-group">
+					<label for="password">Password:</label>
+					<input type="password" class="form-control" id="password" name="password"
+						   value="{{old('password')}}" required>
+				</div>
+
+				<!--Confirmation Password-->
+				<div class="form-group">
+					<label for="password_confirmation">Password Confirmation:</label>
+					<input type="password" class="form-control" id="password_confirmation" name="password_confirmation" value="{{old('password_confirmation')}}" required>
+				</div>
+
+				<!--User Category-->
+				<div class="form-group">
+					<label for="category">User Category:</label>
+					<select class="form-control" name="category" id="category">
+						<option value="Administrator"
+						@if(old('category')=='Administrator')
+							selected="selected"
+						@endif
+						>
+							Administrator
+						</option>
+						<option value="Reception"
+						@if(old('category')=='Reception')
+							selected="selected"
+						@endif
+						>
+							Reception
+						</option>
+						<option value="Housekeeping"
+						@if(old('category')=='Housekeeping')
+							selected="selected"
+						@endif>
+							Housekeeping
+						</option>
+						<option value="Patron"
+						@if(old('category')=='Patron')
+							selected="selected"
+						@endif>
+							Patron
+						</option>
+					</select>
+				</div>
+
+
 				<button type="submit" class="btn btn-primary">Register</button>
-			</div>
 
-		</form>	
-		
-		@include('layouts.errors')
-		
-		<div class="form-group">
+			</form>
+
+			@include('layouts.errors')
+
 			<a href="/">
 				<button type="submit" class="btn btn-default">
 					Log In
 				</button>
 			</a>
+		</div>
+		<div class="col-xs-4">
+
 		</div>
 	</div>
 @endsection
--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,39 +1,41 @@
 @extends('layouts.master')
 @section('content')
-	<div class="col-sm-12">
- 		
-		<h1>Sign In</h1>
-		
-		<hr>
-		
-		<form method="POST" action="/login">
-			
-			{{csrf_field()}}
-			
-			<div class="form-group">
-				<label for="email">Email Address:</label>
-				<input type="email" class="form-control" id="email" name="email" 
-				value="{{old('email')}}">
+	<div class="col-sm-12 login__container">
+		<div class="col-sm-4"></div>
+ 		<div class="col-sm-4 centre-block login__div">
+			<div class="col-sm-12 hotel__img short__div">
+				<h1>Sign In</h1>
 			</div>
 
-			<div class="form-group">
-				<label for="password">Password:</label>
-				<input type="password" class="form-control" id="password" name="password">
-			</div>
-			
-			<div class="form-group">
+			<hr>
+
+			<form method="POST" action="/login">
+
+				{{csrf_field()}}
+
+				<div class="form-group">
+					<label for="email">Email Address:</label>
+					<input type="email" class="form-control" id="email" name="email"
+					value="{{old('email')}}">
+				</div>
+
+				<div class="form-group">
+					<label for="password">Password:</label>
+					<input type="password" class="form-control" id="password" name="password">
+				</div>
+
+
 				<button type="submit" class="btn btn-primary">Sign In</button>
-			</div>
 
-			@include('layouts.errors')
-		</form> 
-		<div class="form-group">
+
+			</form>
 			<a href="/register">
 				<button type="submit" class="btn btn-default">
-					Register
+						Register
 				</button>
 			</a>
+			@include('layouts.errors')
 		</div>
-
+		<div class="col-sm-4"></div>
 	</div>
 @endsection
